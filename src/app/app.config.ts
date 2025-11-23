b083819ef7207d08a5d4410af7e75fdb8dd60459
Gersomsim <gersomhernandez@me.com>
2025-11-23 08:13:46 -0600

    Instalacion de tailwind

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,27 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, isDevMode } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { provideServiceWorker } from '@angular/service-worker';
+import {
+	ApplicationConfig,
+	provideBrowserGlobalErrorListeners,
+	provideZoneChangeDetection,
+	isDevMode,
+} from '@angular/core'
+import { provideRouter } from '@angular/router'
+import { routes } from './app.routes'
+import { provideServiceWorker } from '@angular/service-worker'
+import { diProvider } from '@infra/di/di.provider'
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          }), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          })
-  ]
-};
+	providers: [
+		...diProvider,
+		provideBrowserGlobalErrorListeners(),
+		provideZoneChangeDetection({ eventCoalescing: true }),
+		provideRouter(routes),
+		provideServiceWorker('ngsw-worker.js', {
+			enabled: !isDevMode(),
+			registrationStrategy: 'registerWhenStable:30000',
+		}),
+		provideServiceWorker('ngsw-worker.js', {
+			enabled: !isDevMode(),
+			registrationStrategy: 'registerWhenStable:30000',
+		}),
+	],
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,25 +1,22 @@
-import { Component, inject, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { OfflineDBService } from '@infra/pwa/services/offline-db.service';
-import { SyncService } from '@infra/pwa/services/sync.service';
+import { Component, inject } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { OfflineDBService } from '@infra/pwa/services/offline-db.service'
+import { SyncService } from '@infra/pwa/services/sync.service'
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+	selector: 'app-root',
+	imports: [RouterOutlet],
+	templateUrl: './app.html',
+	styleUrl: './app.scss',
 })
 export class App {
-  private db = inject(OfflineDBService);
-  private syncService = inject(SyncService);
+	private db = inject(OfflineDBService)
+	private syncService = inject(SyncService)
 
-  protected readonly title = signal('agendaMedica');
+	async ngOnInit() {
+		console.log('🚀 PWA con IndexedDB iniciada')
 
-  
-  async ngOnInit() {
-    console.log('🚀 PWA con IndexedDB iniciada');
-    
-    // Limpiar datos antiguos al inicio
-    await this.syncService.cleanup();
-  }
+		// Limpiar datos antiguos al inicio
+		await this.syncService.cleanup()
+	}
 }
