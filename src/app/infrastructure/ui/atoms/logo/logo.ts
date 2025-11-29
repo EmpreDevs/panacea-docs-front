2fbf32477a1ebe5a230c093afa7da564099fe391
Gersomsim <gersomhernandez@me.com>
2025-11-29 00:13:20 -0600

    Fix: ajuste de logo

--- src/app/infrastructure/ui/atoms/logo/logo.ts
+++ src/app/infrastructure/ui/atoms/logo/logo.ts
@@ -1,16 +1,30 @@
-import { Component, input } from '@angular/core'
+import { Component, computed, Input, input } from '@angular/core'
 import { UiIsotipo } from '../ui-isotipo/ui-isotipo'
+import { CommonModule } from '@angular/common'
 
 @Component({
 	selector: 'app-logo',
-	imports: [UiIsotipo],
-	template: ` <div class="flex items-center space-x-3 breathe">
+	imports: [UiIsotipo, CommonModule],
+	template: ` <div class="flex items-center space-x-3">
 		<div class="relative">
-			<app-ui-isotipo />
+			<app-ui-isotipo [size]="size()" />
 		</div>
 		<div>
-			<h2 class="font-sora text-2xl font-bold text-slate-800 dark:text-white">Panacea Docs</h2>
+			<h2 [ngClass]="clases()" class="font-sora text-2xl font-bold text-slate-800 dark:text-white">
+				Panacea Docs
+			</h2>
 		</div>
 	</div>`,
 })
-export class Logo {}
+export class Logo {
+	size = input<'md' | 'lg'>('lg')
+
+	clases = computed(() => {
+		switch (this.size()) {
+			case 'md':
+				return 'text-base'
+			case 'lg':
+				return 'text-3xl'
+		}
+	})
+}
--- src/app/infrastructure/ui/atoms/ui-isotipo/ui-isotipo.ts
+++ src/app/infrastructure/ui/atoms/ui-isotipo/ui-isotipo.ts
@@ -1,13 +1,33 @@
-import { Component, input } from '@angular/core'
+import { Component, computed, input } from '@angular/core'
 import { FileStack, LucideAngularModule } from 'lucide-angular'
+import { NgClass } from '@angular/common'
 
 @Component({
 	selector: 'app-ui-isotipo',
-	imports: [LucideAngularModule],
+	imports: [LucideAngularModule, NgClass],
 	templateUrl: './ui-isotipo.html',
 	styles: ``,
 })
 export class UiIsotipo {
 	light = input<boolean>(false)
+	size = input<'md' | 'lg'>('lg')
 	LogoPanacea = FileStack
+
+	sizeIcon = computed(() => {
+		switch (this.size()) {
+			case 'md':
+				return 18
+			case 'lg':
+				return 32
+		}
+	})
+
+	clasesContainer = computed(() => {
+		switch (this.size()) {
+			case 'md':
+				return 'w-8 h-8 rounded'
+			case 'lg':
+				return 'w-12 h-12 rounded-2xl'
+		}
+	})
 }
