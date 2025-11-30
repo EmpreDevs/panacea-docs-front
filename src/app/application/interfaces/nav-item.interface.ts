7accf3a062fcd6bad8aacd4e4b4be6be3d6cfd5b
Gersomsim <gersomhernandez@me.com>
2025-11-30 15:49:59 -0600

    Interfaces de menu

---
+++ src/app/application/interfaces/nav-item.interface.ts
@@ -0,0 +1,7 @@
+import { LucideIconData } from 'lucide-angular'
+
+export interface INavItem {
+	name: string
+	icon: LucideIconData
+	view: string
+}
--- src/app/application/services/index.ts
+++ src/app/application/services/index.ts
@@ -1,2 +1,3 @@
 export * from './theme.service'
 export * from './breadcrumb.service'
+export * from './menu.service'
