693a3af64bd9a4ae483f189f01d9f6d8bc80d55f
Gersomsim <gersomhernandez@me.com>
2025-11-30 21:36:47 -0600

    Fix: Providers para value accesor

--- src/app/infrastructure/ui/molecules/ui-input/ui-input.ts
+++ src/app/infrastructure/ui/molecules/ui-input/ui-input.ts
@@ -1,11 +1,19 @@
-import { Component, ElementRef, Input, signal, ViewChild } from '@angular/core'
+import { Component, ElementRef, forwardRef, Input, signal, ViewChild } from '@angular/core'
 import { distinctUntilChanged, fromEvent, map, Subject, takeUntil } from 'rxjs'
 import { InputType } from './input.type'
 import { LucideIconData, LucideAngularModule } from 'lucide-angular'
+import { NG_VALUE_ACCESSOR } from '@angular/forms'
 
 @Component({
 	selector: 'app-ui-input',
 	imports: [LucideAngularModule],
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			useExisting: forwardRef(() => UiInput),
+			multi: true,
+		},
+	],
 	templateUrl: './ui-input.html',
 })
 export class UiInput {
