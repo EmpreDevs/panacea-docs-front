56c5373838a2c8e8ddde9da7c980ba1479298566
Gersomsim <gersomhernandez@me.com>
2025-12-01 00:49:35 -0600

    Fix: Sincronizacion con servicio de estado

--- src/app/infrastructure/guards/guest-guard.ts
+++ src/app/infrastructure/guards/guest-guard.ts
@@ -1,18 +1,29 @@
 import { inject } from '@angular/core'
+import { toObservable } from '@angular/core/rxjs-interop'
 import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router'
-import { AuthFacade } from '@infra/store/facades'
+import { AuthState } from '@infra/store/states/auth.state'
+import { filter, map, take } from 'rxjs'
 
 export const guestGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-	const authFacade = inject(AuthFacade)
+	const authState = inject(AuthState)
 	const router = inject(Router)
-	const isAuthenticated = authFacade.isAuthenticated()
 
-	console.log('guest guard ejecutado')
+	const isLoading$ = toObservable(authState.isLoading)
 
-	if (!isAuthenticated()) {
-		return true
-	}
-	router.navigate(['/app'])
+	// 2. Esperar a que la carga inicial termine (isLoading se vuelva false)
+	return isLoading$.pipe(
+		filter(isLoading => isLoading === false), // Espera hasta que isLoading sea false
+		take(1), // Solo necesitamos el primer valor después de la carga
+		map(() => {
+			// 3. Evaluar el estado de autenticación SÓLO después de la carga
+			const isAuthenticated = authState.isAuthenticated()
 
-	return false
+			if (!isAuthenticated) {
+				return true
+			}
+
+			router.navigate(['/app'])
+			return false
+		}),
+	)
 }
--- src/app/infrastructure/guards/logued-guard.ts
+++ src/app/infrastructure/guards/logued-guard.ts
@@ -1,19 +1,28 @@
 import { inject } from '@angular/core'
 import { CanActivateFn, Router } from '@angular/router'
-import { AuthFacade } from '@infra/store/facades'
+import { AuthState } from '@infra/store/states/auth.state'
+import { toObservable } from '@angular/core/rxjs-interop'
+import { filter, map, take } from 'rxjs'
 
 export const loguedGuard: CanActivateFn = (route, state) => {
-	const authFacade = inject(AuthFacade)
+	const authState = inject(AuthState)
 	const router = inject(Router)
-	const isLoggedIn = authFacade.isAuthenticated()
-	console.log('logued guard ejecutado')
-	console.log(route.toString())
+	const isLoading$ = toObservable(authState.isLoading)
 
-	if (isLoggedIn()) {
-		return true
-	}
+	// 2. Esperar a que la carga inicial termine (isLoading se vuelva false)
+	return isLoading$.pipe(
+		filter(isLoading => isLoading === false), // Espera hasta que isLoading sea false
+		take(1), // Solo necesitamos el primer valor después de la carga
+		map(() => {
+			// 3. Evaluar el estado de autenticación SÓLO después de la carga
+			const isAuthenticated = authState.isAuthenticated()
 
-	router.navigate(['/auth'])
+			if (isAuthenticated) {
+				return true
+			}
 
-	return false
+			router.navigate(['/auth'])
+			return false
+		}),
+	)
 }
