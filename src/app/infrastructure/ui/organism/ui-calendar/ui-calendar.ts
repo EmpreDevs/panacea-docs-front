43bbb86303467919fc1c0193506e1862d0ce486c
Gersomsim <gesomhernandez@me.com>
2025-12-14 23:51:56 -0600

    🧱 Componente de calendario

---
+++ src/app/infrastructure/ui/organism/ui-calendar/ui-calendar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-calendar',
+  imports: [],
+  templateUrl: './ui-calendar.html',
+  styles: ``,
+})
+export class UiCalendar {
+
+}
