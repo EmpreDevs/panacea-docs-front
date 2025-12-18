35534c8c4d187095f46ffff9f3dd98a1c8f30ab6
Gersomsim <gesomhernandez@me.com>
2025-12-18 14:34:47 -0600

    Feat: partes de la modal

---
+++ src/app/infrastructure/ui/molecules/ui-modal-body/ui-modal-body.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-modal-body',
+  imports: [],
+  templateUrl: './ui-modal-body.html',
+  styles: ``,
+})
+export class UiModalBody {
+
+}
---
+++ src/app/infrastructure/ui/molecules/ui-modal-footer/ui-modal-footer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-modal-footer',
+  imports: [],
+  templateUrl: './ui-modal-footer.html',
+  styles: ``,
+})
+export class UiModalFooter {
+
+}
